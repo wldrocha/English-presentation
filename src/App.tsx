2b12042924a6e07a3041710046bb49acc3161e83
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-03 13:59:13 -0400

    feat: ✨ my love structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import myPic from './assets/my_pic.png'
-
+import myLovePic from './assets/my_love_pic.jpg'
 
 function App() {
   return (
     <main className='grid grid-cols-2 place-items-center gap-2'>
-      <section className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+      <section className='max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
         <figure className='flex flex-col items-center'>
           <img src={myPic} alt='' className='rounded-full w-9/12' />
           <figcaption>
@@ -15,7 +15,16 @@ function App() {
           </figcaption>
         </figure>
       </section>
-    
+      <section className='max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+        <figure>
+          <img
+            src={myLovePic}
+            alt=''
+            className='object-left-top w-full rounded-t-lg h-96 md:h-auto w-3/6 md:rounded-none md:rounded-s-lg'
+          />
+        </figure>
+        <h2 className=' my-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white'>Alondra</h2>
+      </section>
     </main>
   )
 }
