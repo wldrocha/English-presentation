49e5e88c6f69782d6ee470b5e549bb0c6986299e
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-09 15:37:53 -0400

    feat: ✨ sports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import myPic from './assets/my_pic.png'
 import myLovePic from './assets/my_love_pic.jpg'
+import chessPic from './assets/chess_pic.webp'
+import baseballPic from './assets/ball_pic.webp'
+import stylesHeart from './heart.module.css'
+import stylesSports from './chess.module.css'
 
 function App() {
   return (
@@ -23,7 +27,31 @@ function App() {
             className='object-left-top w-full rounded-t-lg h-96 md:h-auto w-3/6 md:rounded-none md:rounded-s-lg'
           />
         </figure>
+        <div className={stylesHeart['container-heart']}>
+          <div className={stylesHeart.heart} />
+        </div>
         <h2 className=' my-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white'>Alondra</h2>
+      </section>
+      <section className='w-9/12 h-96 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center'>
+        <div className='flex justify-center w-full'>
+          <figure className='flex items-center'>
+            <img
+              src={chessPic}
+              alt=''
+              className={`${stylesSports.chess} object-left-top w-full  rounded-t-lg h-96 md:h-auto w-40 md:rounded-none md:rounded-s-lg`}
+            />
+          </figure>
+          <figure className='flex items-center'>
+            <img
+              src={baseballPic}
+              alt=''
+              className={`${stylesSports.baseball} object-left-top w-full  rounded-t-lg h-96 md:h-auto w-40 md:rounded-none md:rounded-s-lg`}
+            />
+          </figure>
+        </div>
+        <h2 className='mt-4 my-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white'>
+          Sports
+        </h2>
       </section>
     </main>
   )
