04b20080ef25dd84f027065ad216e047c4a17a7b
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-10 20:44:52 -0400

    feat: 💄 change file name styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import chessPic from './assets/chess_pic.webp'
 import baseballPic from './assets/ball_pic.webp'
 import momPic from './assets/mom_pic.jpg'
 import dadPic from './assets/dad_pic.jpg'
+import pizzaPic from './assets/pizza_pic.png'
+import passionFruitPic from './assets/passion_fruit_pic.png'
 import stylesHeart from './heart.module.css'
 import stylesSports from './sports.module.css'
+import stylesPet from './pet.module.css'
 
 function App() {
   return (
@@ -68,6 +71,20 @@ function App() {
           Parents
         </h2>
       </section>
+      <section className='w-9/12 h-96 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center'>
+        <div className='flex justify-center w-full'>
+          <figure className='flex items-center overflow-hidden size-72'>
+            <img src={pizzaPic} alt='' className={` object-top w-full h-96 md:h-auto w-40 `} />
+          </figure>
+          <figure className='flex items-center overflow-hidden size-72'>
+            <img src={passionFruitPic} alt='' className={` object-top w-full h-96 md:h-auto w-40 `} />
+          </figure>
+        </div>
+        <h2 className='mt-4 my-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white'>
+          Parents
+        </h2>
+      </section>
+  
     </main>
   )
 }
