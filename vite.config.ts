196b04f069f06ea801ba9de8a42270ed728edb3b
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-11 16:04:27 -0400

    chore: Update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/wldrocha/english-presentation/'
+  base: 'https://wldrocha.github.io/english-presentation/'
 })
