1ef38f4f0ea23096d178e880e7cd46b823c20c39
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-11 16:28:43 -0400

    chore: Update npm dependencies and add vite-plugin-imagemin for image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vitePluginImp from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginImp({
+      // Desactiva los plugins que no necesitas
+      gifsicle: false,
+      optipng: false,
+      mozjpeg: false,
+      pngquant: false,
+      svgo: false,
+      // Activa y configura el plugin webp
+      webp: {
+        quality: 75
+      }
+    })
+  ],
   base: 'https://wldrocha.github.io/english-presentation/'
 })
